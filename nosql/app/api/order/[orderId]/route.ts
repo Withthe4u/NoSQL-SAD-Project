6fbad4a5ad1b411bb3955e06d8a4dafa5271bb1f
Withthe4u <phuwaritkunlapan80@gmail.com>
2026-04-06 22:56:08 +0700

    feature/updateOrderStatus-Phu

--- nosql/app/api/order/[orderId]/route.ts
+++ nosql/app/api/order/[orderId]/route.ts
@@ -1,12 +1,43 @@
 import { NextRequest, NextResponse } from "next/server";
+import { updateOrderStatusRepo } from "@/server/repositories/order.repo";
+import { UpdateOrderStatusSchema } from "@/server/models";
+import { handleError } from "@/server/presentation/response";
 
-/* //update order status only naja
-export async function PATCH(req:NextRequest, context:{params:Promise<{orderId:string}>}){
-    const {orderId} = await context.params
-    try {
-        
-    } catch (error) {
-        
+export async function PATCH(
+  req: NextRequest,
+  context: { params: Promise<{ orderId: string }> }
+) {
+  try {
+    const { orderId } = await context.params;
+
+    const body = await req.json();
+    const parsed = UpdateOrderStatusSchema.pick({
+      status: true,
+    }).safeParse(body);
+
+    if (!parsed.success) {
+      return NextResponse.json(
+        { message: "Invalid input" },
+        { status: 400 }
+      );
     }
+
+    const { status } = parsed.data;
+
+    const updatedOrder = await updateOrderStatusRepo({
+      orderId,
+      status,
+    });
+
+    if (!updatedOrder) {
+      return NextResponse.json(
+        { message: "Order not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(updatedOrder, { status: 200 });
+  } catch (error) {
+    return handleError(error);
+  }
 }
-*/
